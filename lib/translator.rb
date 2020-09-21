f68c54c3de6ed9f9aa02ddca2c5b178ccbf8499c
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-21 10:38:33 -0600

    add attr_reader and initialize to translator class (was using base init)

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,2 +1,12 @@
+require "./lib/encodable"
+
 class Translator
+  include Encodable
+  
+  attr_reader :input_text, :output_text
+
+  def initialize(input_text)
+    @input_text = input_text
+    @output_text = nil
+  end
 end
