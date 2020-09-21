0da059d58bbc44cda39056f35a10d44e7d499ec9
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-21 10:37:55 -0600

    Add attr tests for Translator class

--- test/translator_test.rb
+++ test/translator_test.rb
@@ -2,10 +2,15 @@ require "./test/test_helper"
 
 class TranslatorTest < Minitest::Test
   def setup
-    @translator = Translator.new
+    @translator = Translator.new(["a", "b", "c", "d", "e"])
   end
 
   def test_it_is_translator
     assert_instance_of Translator, @translator
+  end
+
+  def test_it_has_attributes
+    assert_equal ["a", "b", "c", "d", "e"], @translator.input_text
+    assert_nil @translator.output_text
   end
 end
