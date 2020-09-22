7448bef8de6e727c612553f6f82579fa7c1e3c5e
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-22 06:09:39 -0600

    Fix deciphertest assertions and include decipher in test_helper

--- test/decipher_test.rb
+++ test/decipher_test.rb
@@ -2,7 +2,7 @@ require "./test/test_helper"
 
 class DecipherTest < Minitest::Test
   def setup
-    @decrypto = Decipher.new
+    @decrypto = Decipher.new("keder ohulw", "02715", "040895")
   end
 
   def test_it_is_a_decipherer
