2837e4028643451a79fedb1d9b67d7d220ebf7e5
Hope Gochnour <hope.gochnour@gmail.com>
2020-09-22 06:22:53 -0600

    Add test to DecipherTest to make sure that shift_keys can be negative

--- test/decipher_test.rb
+++ test/decipher_test.rb
@@ -9,8 +9,15 @@ class DecipherTest < Minitest::Test
     assert_instance_of Decipher, @decrypto
   end
 
+  def test_shift_keys_can_be_negative
+    assert_equal (-2), @decrypto.a
+    assert_equal (-27), @decrypto.b
+    assert_equal (-71), @decrypto.c
+    assert_equal (-15), @decrypto.d
+  end
+
   def test_it_can_decrypt_messages
     @decrypto.process
-    assert_equal "hello world", @decrypto.final_text 
+    assert_equal "hello world", @decrypto.final_text
   end
 end
